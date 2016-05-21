49056c2abd125aa2f80b61c9997e3d5753a52638
scottpschaffer <scottpschaffer@yahoo.com>
2016-05-21 10:12:45 -0500

    first commit

--- app/app.js
+++ app/app.js
@@ -1,10 +1,20 @@
 var app = angular.module("TodoApp", []);
 
 app.controller("NavCtrl", function($scope){
-  $scope.navItems = [{name: "Logout"},{name: "All Items"}, {name: "New Item"}]
-})
+  $scope.navItems = [{name: "Logout"},{name: "All Items"}, {name: "New Item"}];
+});
 
 app.controller("TodoCtrl", function($scope){
   $scope.welcome = "Hello";
+  $scope.showListView = true;
 
-})
+  $scope.newItem = function(){
+    console.log("you clicked new Item");
+    $scope.showListView = true;
+  };
+  $scope.allItem = function(){
+    console.log("You clicked All Items");
+    $scope.showListView = false;
+
+  };
+});
