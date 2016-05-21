eb1e90d0f00974c36ab31952fbbbdfe977df328b
scottpschaffer <scottpschaffer@yahoo.com>
2016-05-21 11:09:32 -0500

    Second Commit

--- app/app.js
+++ app/app.js
@@ -7,6 +7,41 @@ app.controller("NavCtrl", function($scope){
 app.controller("TodoCtrl", function($scope){
   $scope.welcome = "Hello";
   $scope.showListView = true;
+  $scope.NewTask={};
+
+
+  $scope.items = [
+    {
+      id: 0,
+      task: "mow the lawn",
+      isCompleted: true,
+      dueDate: "12/5/17",
+      assignedTo: "Greg",
+      location: "Zoe's house",
+      urgency: "low",
+      dependencies: ["sunshine, clippers, hat, water, headphones"]
+    },
+    {
+      id: 1,
+      task: "grade quizzes",
+      isCompleted: false,
+      dueDate: "12/12/17",
+      assignedTo: "Joe",
+      location: "NSS",
+      urgency: "high",
+      dependencies: ["wifi, tissues, vodka"]
+    },
+    {
+      id: 2,
+      task: "take a nap",
+      isCompleted: false,
+      dueDate: "12/5/17",
+      assignedTo: "Zoe",
+      location: "Zoe's house",
+      urgency: "low",
+      dependencies: ["sunshine, clippers, hat, water, headphones"]
+    }
+  ]
 
   $scope.newItem = function(){
     console.log("you clicked new Item");
@@ -15,6 +50,12 @@ app.controller("TodoCtrl", function($scope){
   $scope.allItem = function(){
     console.log("You clicked All Items");
     $scope.showListView = false;
-
   };
+  $scope.addNewItem = function(){
+    $scope.NewTask.isCompleted = false;
+    $scope.NewTask.id = $scope.items.length;
+    console.log("You added a new Item", $scope.NewTask);
+    $scope.items.push($scope.NewTask);
+    $scope.NewTask="";
+  }
 });
