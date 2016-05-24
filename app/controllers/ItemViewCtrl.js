7064c94f9b46a179eb4541ab21320be3d238b7f3
scottpschaffer <scottpschaffer@yahoo.com>
2016-05-23 20:52:38 -0500

    Added Details page

--- app/controllers/ItemViewCtrl.js
+++ app/controllers/ItemViewCtrl.js
@@ -1,4 +1,22 @@
-app.controller("ItemViewCtrl", function($scope){
-  $scope.welcome = "Hello";
+app.controller("ItemViewCtrl", function($scope, $http, $routeParams){
+  $scope.items=[];
+  $scope.selectedItem = {};
+  console.log($routeParams.itemId);
+      
+  $http.get("https://todo-appz2-scott.firebaseio.com/items.json")
+    .success(function(itemObject){
+      var itemCollection = itemObject;
+      // var itemCollection = itemObject.items;
+      console.log("itemObject", itemObject);
+        Object.keys(itemCollection).forEach(function(key){
+          itemCollection[key].id=key;
+          $scope.items.push(itemCollection[key]);
 
+          $scope.selectedItem = $scope.items.filter(function(item){
+            return item.id === $routeParams.itemId;
+          })[0];
+
+
+        })
+  });
 });
